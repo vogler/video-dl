abb4dfa1ef722d33c15b6fb052ff0dd25eca4d12
Ralf Vogler <ralf.vogler@gmail.com>
2020-09-09 11:45:16 +0200

    do not try slow youtube-dl if http status is not 200

--- news-dl.sh
+++ news-dl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+
 # run this script as a cronjob to have news to watch for breakfast
 # $ crontab -e
 # 0 22   *   *   *     ~/news/news-dl.sh
@@ -8,14 +10,18 @@
 mkdir -p videos
 cd "$(dirname "$0")/videos"
 
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-clip # e.g. 01-01-2020
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-clip2 # happend on 12-06-2020
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-ganze-folge # sometimes the URL is different, only Tuesdays?
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%B-%Y')-clip # on 09.04.2020 they changed to full month name: 09-april-2020-clip
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%-d-%-m-%Y')-clip # e.g. 1-1-2020; on 18.04.2020 they changed to non-leading zero for month: 18-4-2020-clip
-youtube-dl https://www.sat1.de/news/video/abendnachrichten-$(date '+%-d-%-m-%Y')-ganze-folge # 19-4-2020-ganze-folge
+function ytdl { # abort before youtube-dl if http status code is not 200; not needed, but faster than letting youtube-dl try first
+  curl -o /dev/null --silent --head --write-out '%{http_code}\n' $1 | grep -q 200 && youtube-dl $1
+}
 
-youtube-dl https://www.tagesschau.de/sendung/tagesschau/index.html
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-clip # e.g. 01-01-2020
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-clip2 # happend on 12-06-2020
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%m-%Y')-ganze-folge # sometimes the URL is different, only Tuesdays?
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%d-%B-%Y')-clip # on 09.04.2020 they changed to full month name: 09-april-2020-clip
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%-d-%-m-%Y')-clip # e.g. 1-1-2020; on 18.04.2020 they changed to non-leading zero for month: 18-4-2020-clip
+ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%-d-%-m-%Y')-ganze-folge # 19-4-2020-ganze-folge
+
+ytdl https://www.tagesschau.de/sendung/tagesschau/index.html
 
 # The following does not work because there is only an image instead of a video element until the teaser is clicked on:
 # youtube-dl https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html
