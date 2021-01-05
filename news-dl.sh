d6bbda1778e8ea893567874ec5ef1fe742b4dda0
Ralf Vogler <ralf.vogler@gmail.com>
2021-01-05 17:17:32 +0100

    Börse vor Acht moved from boerse.ard.de to tagesschau.de on 15.12.20

--- news-dl.sh
+++ news-dl.sh
@@ -23,15 +23,20 @@ ytdl https://www.sat1.de/news/video/abendnachrichten-$(date '+%-d-%-m-%Y')-ganze
 
 ytdl https://www.tagesschau.de/sendung/tagesschau/index.html
 
-# The following does not work because there is only an image instead of a video element until the teaser is clicked on:
-# youtube-dl https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html
-# Since the url has some number suffix which we can't guess, we have to extract the latest url from the index page first (use cheerio-cli from submodule where piping is fixed):
-# youtube-dl $(curl -s https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html | ../cheerio-cli/bin/cheerio '.teaser a' -a href | head -n1)
-# This works but youtube-dl only downloads the lowest resolution...
-# DIY:
-latest=$(curl -s https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html | grep -o 'https.*boerse-vor-acht/hr_.*\.html' | head -n1)
-wget -nc $(curl -s $latest | grep -o "https://.*1280x720-50p-5000kbit\.mp4")
-
+# changed on 05.01.21 since 'boerse.ard.de zieht zu tagesschau.de (15.12.20)'
+# old URL was https://www.daserste.de/information/wirtschaft-boerse/boerse-im-ersten/videosextern/index.html
+# can't find any 'Börse vor Acht' on tagesschau.de, but there's
+# https://www.daserste.de/information/wirtschaft-boerse/boerse-im-ersten/videosextern/index.html
+# Since this is an overview and videos have a number suffix which we can't guess, we have to extract the latest URL from the index page first.
+# old:
+  # use cheerio-cli from submodule where piping is fixed:
+  # youtube-dl $(curl -s https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html | ../cheerio-cli/bin/cheerio '.teaser a' -a href | head -n1)
+  # This works but youtube-dl only downloads the lowest resolution...
+  # DIY:
+  # latest=$(curl -s https://boerse.ard.de/multimedia/audios-und-videos/boerse-vor-acht/index.html | grep -o 'https.*boerse-vor-acht/hr_.*\.html' | head -n1)
+  # wget -nc $(curl -s $latest | grep -o "https://.*1280x720-50p-5000kbit\.mp4")
+# new:
+youtube-dl "https://daserste.de$(curl -s https://www.daserste.de/information/wirtschaft-boerse/boerse-im-ersten/videosextern/index.html | grep -o '/.*boerse-vor-acht-video-.*\.html' | head -n1)"
 
 # delete files with an access time older than 7 days
 find . -type f -atime +7 -exec rm -f {} \;
